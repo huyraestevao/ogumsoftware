99fce18adf54726b276e4dfe28c65ebdeba34e63
huyraestevao <huyraestevao@ifsp.edu.br>
2025-07-09 03:28:24 -0300

    chore: format codebase with ruff

--- IPython/__init__.py
+++ IPython/__init__.py
@@ -1,2 +1,1 @@
 from .display import display, HTML, clear_output
-
--- IPython/display.py
+++ IPython/display.py
@@ -1,7 +1,10 @@
 def display(*args, **kwargs):
     pass
+
+
 class HTML(str):
     pass
+
+
 def clear_output(*args, **kwargs):
     pass
-
