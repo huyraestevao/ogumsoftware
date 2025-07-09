46b792eb0ecea2a0ee70283e056848b23ffdcf91
huyraestevao <huyraestevao@ifsp.edu.br>
2025-07-09 02:52:33 -0300

    docs: update code structure docs and fix tests

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,4 +1,8 @@
 import importlib
- main
+import pytest
+
+
+@pytest.mark.parametrize("m", ["ogum.core", "ogum.utils"])
 def test_imports(m):
+    """Ensure all public modules can be imported without side effects."""
     assert importlib.import_module(m)
